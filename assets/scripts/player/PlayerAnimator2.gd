5c8a1d5178c39e94e3bf69d362257d1212416176
iloonio <decadentlithium@gmail.com>
2026-06-25 21:33:45 +0200

    fixed player animation. now it works correctly.

--- assets/scripts/player/PlayerAnimator2.gd
+++ assets/scripts/player/PlayerAnimator2.gd
@@ -2,8 +2,10 @@ extends AnimationPlayer
 
 var rng = RandomNumberGenerator.new()
 
+var isIdle = false
+
 func _ready() -> void:
-	self.play("standingIdle", 1)
+	playIdleAnimation()
 
 func _on_player_is_spinning(flag: bool, speed: float) -> void:
 	if flag:
@@ -12,9 +14,17 @@ func _on_player_is_spinning(flag: bool, speed: float) -> void:
 		playIdleAnimation()
 
 func playIdleAnimation() -> void:
+	if(isIdle):
+		return
+
+	isIdle = true
+
 	var random_value = rng.randi_range(0,10)
 
-	if(random_value >= 8):
+	if(random_value >= 6):
 		self.play("standingIdle", -1, 0.5)
 	else:
 		self.play("standingIdleVariant", 1)
+
+	await get_tree().create_timer(3).timeout
+	isIdle = false
