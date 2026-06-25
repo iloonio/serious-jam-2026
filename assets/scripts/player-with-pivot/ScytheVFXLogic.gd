bdf5a021b53c7c6e34ae5a861eb70c72b150bd1e
iloonio <decadentlithium@gmail.com>
2026-06-25 04:08:13 +0200

    added a bunch of empty scripts These are all related to the rehauled player with pivot

---
+++ assets/scripts/player-with-pivot/ScytheVFXLogic.gd
@@ -0,0 +1,12 @@
+extends CPUParticles3D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	transform.rotated(Vector3.UP, deg_to_rad(15))
+	
+
+
+func _on_player_velocity(currentVelocity: float) -> void:
+	if(currentVelocity < 0.1):
+		emitting = false
+	else: emitting = true
