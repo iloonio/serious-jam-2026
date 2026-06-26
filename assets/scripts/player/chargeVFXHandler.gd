8be02f45f19d7788d07f2387047b1163d0f803c9
iloonio <decadentlithium@gmail.com>
2026-06-26 15:23:55 +0200

    Added VFX logic for charging and releasing

---
+++ assets/scripts/player/chargeVFXHandler.gd
@@ -0,0 +1,65 @@
+extends CPUParticles3D
+
+## all fx that will be played when player is charging up (there is 2 of them)
+## Group name:
+	## ChargeUpVFX
+var chargeUpVFX: Array = [Node3D]
+
+## fx that plays on charge release. there should only be one but just in case
+## more are added this is kept as an array
+## Group name:
+	## ChargeReleaseVFX
+var chargeReleaseVFX: Array = [Node3D]
+
+## all fx that play when a perfect charge is performed
+## Group name:
+	## PerfectChargeVFX
+var perfectChargeVFX: Array = [Node3D]
+
+func _ready() -> void:
+	# get all the children
+	chargeUpVFX = get_tree().get_nodes_in_group("ChargeUpVFX")
+
+	chargeReleaseVFX = get_tree().get_nodes_in_group("ChargeReleaseVFX")
+
+	perfectChargeVFX = get_tree().get_nodes_in_group("PerfectChargeVFX")
+
+	# thats it!
+
+
+
+# its important to note that chargeUpVFX are not set to OneShot, so they have to be manually turned off
+# when the player releases charge
+func _on_player_charge(currentCharge: float, minThreshold: float, maxThreshold: float) -> void:
+	# we want to emit all VFX when the player charges.
+	for vfx in chargeUpVFX:
+		if vfx is CPUParticles3D:
+			vfx.one_shot = false
+			vfx.emitting = true
+			# Scale emission rate based on charge progress
+			# var chargeProgress = (currentCharge - minThreshold) / (maxThreshold - minThreshold)
+			# vfx.amount_ratio = clamp(chargeProgress, 0.3, 1.0)
+
+
+func _on_player_charge_release(isChargePerfect: bool) -> void:
+	# we want to set chargeUpVFX particles to be Oneshot when this is called
+	# once these finish playing, they should be set back to not being oneshot.
+	# we want to emit release VFX when the player releases charge.
+	#
+	print(isChargePerfect)
+
+
+
+	if isChargePerfect:
+		# we want to emit perfectChargeVFX in the case of a perfectCharge
+		for vfx in perfectChargeVFX:
+			if vfx is CPUParticles3D:
+				vfx.emitting = true
+
+	# Set chargeUpVFX to OneShot mode so they finish naturally
+	for vfx in chargeUpVFX:
+		if vfx is CPUParticles3D:
+			vfx.one_shot = true
+			# Wait for this particle to finish, then reset OneShot mode
+			await vfx.finished
+			vfx.one_shot = false
